642c095194fb39068ca0a92b24aa0d2bea249f91
MSDN-SPECIAL\125 <kachounghoa@naver.com>
2016-08-30 16:33:59 +0900

    no message

--- th125/th125_Jsp_servlet/workspace/dd/src/dd/dddd.java
+++ th125/th125_Jsp_servlet/workspace/dd/src/dd/dddd.java
@@ -1,16 +1,9 @@
 package dd;
 
 public class dddd {
-<<<<<<< HEAD
-	String arc="ssss";
-=======
-<<<<<<< HEAD
-	String arc;
+	String arc = "ssss";
+
 	public static void main(String[] args) {
 		System.out.println("김다솜");
 	}
-=======
-	String arc="";
->>>>>>> 8750da6cf320194d57315467834064dc8325146d
->>>>>>> 5ec707b38fa1a09f62411aafc38fa962b967b6f4
 }
