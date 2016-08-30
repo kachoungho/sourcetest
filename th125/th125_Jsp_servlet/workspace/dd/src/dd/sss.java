ff018a83a9106077bc65897c391cb388989a5472
dasom <oiiuiio@naver.com>
2016-08-30 16:10:43 +0900

    수정함

--- th125/th125_Jsp_servlet/workspace/dd/src/dd/sss.java
+++ th125/th125_Jsp_servlet/workspace/dd/src/dd/sss.java
@@ -4,6 +4,6 @@ public class sss {
 public static void main(String[] args) {
 	System.out.println("dd");
 	System.out.println("dffffff");
-	System.out.println("나는 이쁜 다솜");
+	System.out.println("나는 ");
 }
 }
