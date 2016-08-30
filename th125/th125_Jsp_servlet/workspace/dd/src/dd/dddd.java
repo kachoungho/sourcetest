8ca0da9ba937c0294edff0d80e4ba7ebde364236
13-HP\13 <wltkdehd0@gmail.com>
2016-08-30 16:32:06 +0900

    우와

--- th125/th125_Jsp_servlet/workspace/dd/src/dd/dddd.java
+++ th125/th125_Jsp_servlet/workspace/dd/src/dd/dddd.java
@@ -1,16 +1,9 @@
 package dd;
 
 public class dddd {
-<<<<<<< HEAD
-	String arc="ssss";
-=======
-<<<<<<< HEAD
-	String arc;
+
 	public static void main(String[] args) {
-		System.out.println("김다솜");
+		System.out.println("dddd");
 	}
-=======
-	String arc="";
->>>>>>> 8750da6cf320194d57315467834064dc8325146d
->>>>>>> 5ec707b38fa1a09f62411aafc38fa962b967b6f4
+
 }
